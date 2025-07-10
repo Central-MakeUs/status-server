50802dee8a6503cef8a11de21d3e004e7355ce22
5ooyeon <symelcho@inu.ac.kr>
2025-07-10 17:05:06 +0900

    Feat: add Entity-attribute

---
+++ src/main/java/com/statoverflow/status/domain/attribute/entity/Attribute.java
@@ -0,0 +1,40 @@
+package com.statoverflow.status.domain.attribute.entity;
+
+import com.statoverflow.status.domain.attribute.enums.AttributeType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "attribute")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Attribute {
+
+	@Id
+	private Integer bitMask;
+
+	@Column(nullable = false, unique = true)
+	private Integer attributeId;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private AttributeType type;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private Integer defaultLevel;
+
+}
