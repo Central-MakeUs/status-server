f14861f15a2ef3ff49668c7a3672b93fe216ca69
5ooyeon <symelcho@inu.ac.kr>
2025-07-31 00:06:56 +0900

    Refactor: 테마 호출 메서드 내 중복 로직 메서드화

--- src/main/java/com/statoverflow/status/domain/master/entity/QuestTheme.java
+++ src/main/java/com/statoverflow/status/domain/master/entity/QuestTheme.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(of = "id")
 public class QuestTheme {
 
 	@Id
