d2ebcb9b60684e86f7b2ef25674d33cc624affe0
5ooyeon <melthetensai@gmail.com>
2025-08-02 15:08:41 +0900

    Docs: ProviderType Guide 추가

--- src/main/java/com/statoverflow/status/domain/auth/dto/OAuthLoginRequestDto.java
+++ src/main/java/com/statoverflow/status/domain/auth/dto/OAuthLoginRequestDto.java
@@ -2,9 +2,11 @@ package com.statoverflow.status.domain.auth.dto;
 
 import com.statoverflow.status.domain.users.enums.ProviderType;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 public record OAuthLoginRequestDto(
+        @Schema(description = "로그인 소셜 플랫폼 (KAKAO, GOOGLE, APPLE 중 하나)")
         ProviderType provider,
         String code
 ) {
