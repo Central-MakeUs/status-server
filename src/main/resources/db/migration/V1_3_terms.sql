4e1dcf2c5d5d9c6cb037f749953ded41380f4253
5ooyeon <symelcho@inu.ac.kr>
2025-08-05 22:53:01 +0900

    Chore: db 내 terms 추가

---
+++ src/main/resources/db/migration/V1_3_terms.sql
@@ -0,0 +1,20 @@
+INSERT INTO terms_and_conditions VALUES (
+                                            default,
+                                            NOW(),
+                                            NOW(),
+                                            TRUE,
+                                            'https://www.notion.so/242b86814a1180eeb5d0ee670aa26aa7?pvs=13',
+                                            'SERVICE_TERMS',
+                                            NOW(),
+                                            '1.0.0'
+                                        ),
+                                        (
+                                            default,
+                                            NOW(),
+                                            NOW(),
+                                            TRUE,
+                                            'https://www.notion.so/244b86814a118059abf1c4995f3cd78a',
+                                            'PRIVATE_POLICY',
+                                            NOW(),
+                                            '1.0.0'
+                                        );
