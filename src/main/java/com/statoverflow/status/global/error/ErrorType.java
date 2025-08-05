d2a7e8c50f55e9b71e0bbe01276b4ad4cd0c80f9
5ooyeon <symelcho@inu.ac.kr>
2025-08-05 23:23:34 +0900

    Feat: api - mainQuest Id로 mainQuest 조회

--- src/main/java/com/statoverflow/status/global/error/ErrorType.java
+++ src/main/java/com/statoverflow/status/global/error/ErrorType.java
@@ -23,6 +23,9 @@ public enum ErrorType {
 
     INVALID_ATTRIBUTES(HttpStatus.BAD_REQUEST, "03-001", "한 개나 두 개의 능력치를 선택해야 합니다."),
 
+    // 메인 퀘스트 에러 (02)
+    MAINQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "04-001", "해당하는 메인 퀘스트가 존재하지 않습니다."),
+
     // 서브 퀘스트 에러
     INVALID_SUBQUEST_SELECTED(HttpStatus.BAD_REQUEST, "05-001", "선택된 서브퀘스트의 갯수가 잘못되었습니다."),
     COMPLETED_SUBQUEST(HttpStatus.BAD_REQUEST, "05-002", "이미 완료한 서브퀘스트 입니다.")
