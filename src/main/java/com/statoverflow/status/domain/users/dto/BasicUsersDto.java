fcfffcfb94e2765f508ef6b7a8ff33034cc0167c
Sooyeon Cho <131759990+5ooyeon@users.noreply.github.com>
2025-08-19 21:57:48 +0900

    Feat: 통합 레벨 dto (#51)

--- src/main/java/com/statoverflow/status/domain/users/dto/BasicUsersDto.java
+++ src/main/java/com/statoverflow/status/domain/users/dto/BasicUsersDto.java
@@ -2,15 +2,17 @@ package com.statoverflow.status.domain.users.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.statoverflow.status.domain.auth.dto.SocialLoginReturnDto;
+import com.statoverflow.status.domain.master.enums.Tier;
 import com.statoverflow.status.domain.users.entity.Users;
 import lombok.ToString;
 
 
-public record BasicUsersDto(Long id, String nickname) implements SocialLoginReturnDto {
+public record BasicUsersDto(Long id, String nickname, TierDto tier) implements SocialLoginReturnDto {
 	public static BasicUsersDto from(Users user) {
 		return new BasicUsersDto(
 			user.getId(),
-			user.getNickname()
+			user.getNickname(),
+			new TierDto(Tier.BRONZE, 1)
 		);
 	}
 
---
+++ src/main/java/com/statoverflow/status/domain/users/dto/TierDto.java
@@ -0,0 +1,7 @@
+package com.statoverflow.status.domain.users.dto;
+
+import com.statoverflow.status.domain.master.enums.Tier;
+
+public record TierDto(Tier tier, int level) {
+
+}
